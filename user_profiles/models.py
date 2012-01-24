bf13ce8db3fde179dab5fa12b7676390fb8ba726
root <root@acga@collegiatego.org>
2012-01-24 20:19:54 +0300

    Revert ""Profiles and registration working""
    This reverts commit 654ba2215466e5740517956b002492bc01d39516.

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -4,20 +4,19 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    description = models.TextField(null=True, blank=True)
-    club = models.CharField(max_length=150, null=True, blank=True)
-    handles = models.CharField(max_length=150, null=True, blank=True)
+    description = models.TextField()
+    club = models.CharField(max_length=100)
+    handles = models.CharField(max_length=100)
     aga = models.BooleanField()
     
     def __unicode__(self):
        return self.user.username
 
 def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            print('creating user profile through models')
-            UserProfile.objects.create(user=instance)
+    if created:
+        UserProfile.objects.create(user=instance)
             
-post_save.connect(create_user_profile, sender=User, dispatch_uid="CGA.user_profiles.models")
+post_save.connect(create_user_profile, sender=User)
 
 #class Club(models.Model):
 #    name = models.CharField(max_length=100)
