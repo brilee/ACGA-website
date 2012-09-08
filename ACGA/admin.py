7d0421f5cafdde0e62b255b33dc3df092edeb652
brilee <collegiatego@gmail.com>
2012-09-08 09:03:56 +0400

    Added Document model to ACGA

--- ACGA/admin.py
+++ ACGA/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from CGA.ACGA.models import *
 
 admin.site.register(Newsfeed)
+admin.site.register(Document)
--- ACGA/models.py
+++ ACGA/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+
 
 class Newsfeed(models.Model):
     title = models.CharField(max_length=100)
@@ -13,3 +15,16 @@ class Newsfeed(models.Model):
     def __unicode__(self):
         return unicode('%s published on %s' % (self.title, self.pub_date.strftime('%Y-%B-%d')))
 
+class Document(models.Model):
+    def upload_location(instance, filename):
+        return os.path.join('Documents', filename)
+    
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300, help_text='A brief summary of the document')
+    author = models.CharField(max_length=50)
+    upload = models.FileField(upload_to=upload_location, help_text='Upload file here. PDF strongly preferred.')
+    uploaded = models.DateTimeField(auto_now=True)
+    public = models.BooleanField(default=False, help_text='Should this document be accessible by anyone?')
+
+    def __unicode__(self):
+        return unicode('%s by %s' %(self.title, self.author))
