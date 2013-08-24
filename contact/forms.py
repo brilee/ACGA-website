e7301314a0b4037446e5c05179c08f7e12570ca1
brilee <brian.kihoon.lee@gmail.com>
2013-08-24 19:47:03 +0400

    Added recaptcha to submission forms

---
+++ contact/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from captcha.fields import ReCaptchaField
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=50)
+    email = forms.EmailField(required=False, label='Your email')
+    message = forms.CharField(widget=forms.Textarea)
+    captcha = ReCaptchaField()
