149edc0050bd6a77979566b506cec19f3f2bbb4d
Brian Lee <brian.kihoon.lee@gmail.com>
2015-01-03 10:33:09 -0500

    More migrating ACGA portal to Hubspot

--- urls.py
+++ urls.py
@@ -22,14 +22,6 @@ urlpatterns = patterns('',
         {'document_root': settings.ADMIN_MEDIA_ROOT}),
 )
 
-urlpatterns += patterns('contact.views',
-    # Contact page
-    (r'^contact/$', 'contact'),
-    (r'^contact/thanks/$', TemplateView.as_view(template_name='thanks.html')),
-    (r'^CGL/join/$', 'join_CGL'),
-    (r'^CGL/join/thanks/$', TemplateView.as_view(template_name='thanks_CGL.html')),
-)
-
 urlpatterns += patterns('',
     # Account registration/login
     (r'^accounts/', include('accounts.urls')),
@@ -50,6 +42,8 @@ urlpatterns += patterns('',
     (r'^resources/$', my_redirect('http://www.college-go.org/resources')),
     (r'^ing/$', my_redirect('http://www.college-go.org/ing-foundation')),
     (r'^about/$', my_redirect('http://www.college-go.org/about-us')),
+    (r'^contact/?$', my_redirect('http://www.college-go.org/contact-us')),
+    (r'^members/?$', my_redirect('/CGL/schools/')),
     (r'^events/$', my_redirect('http://www.college-go.org/events')),
     (r'^/events/spring_open_2012/$', my_redirect('http://www.college-go.org/events/2012-acga-spring-go-open')),
     (r'^/events/spring_expo_2013/$', my_redirect('http://www.college-go.org/events/2012-acga-spring-go-open')),
