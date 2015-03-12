2fa842a6ff4f7683c16ce62c1a4049091cedd93f
Brian Lee <brian.kihoon.lee@gmail.com>
2015-03-12 00:26:17 -0400

    change season

--- CGL/settings.py
+++ CGL/settings.py
@@ -1,2 +1,4 @@
-current_seasons = ['Season Four']
-current_ladder_season = 'Season Four'
+# this one controls all the seasons that display on the Results tab
+current_seasons = ['Season Four Championship']
+# this determines default of management commands for updating scores
+current_ladder_season = 'Season Four Championship'
