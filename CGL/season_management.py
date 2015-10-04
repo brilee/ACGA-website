b9b2efa8ddc3cf63fbd7dc2d67b24219858bb333
Brian Lee <brian.kihoon.lee@gmail.com>
2015-10-04 12:29:26 -0400

    more efficient query

--- CGL/season_management.py
+++ CGL/season_management.py
@@ -1,9 +1,6 @@
-from CGL.models import Team, Season
+from CGL.models import Team, School
 
 def get_actively_participating_schools(current_season_names):
-    current_seasons = Season.objects.filter(name__in=current_season_names)
-    participating_schools = set()
-    for season in current_seasons:
-        participating_schools = (participating_schools | set(m.school for m in Team.objects.filter(season=season)))
+    school_ids = [d['school'] for d in Team.objects.filter(season__name__in=current_season_names).values("school")]
 
-    return participating_schools
+    return School.objects.filter(id__in=school_ids)
