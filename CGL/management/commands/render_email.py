8721c3859cd2e4f29c04ad4f357097d43cdd8fd3
Brian Lee <brian.kihoon.lee@gmail.com>
2015-10-07 23:25:41 -0400

    restructure email sending

--- CGL/management/commands/render_email.py
+++ CGL/management/commands/render_email.py
@@ -1,20 +1,49 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.template import Context, loader
+import os
 
-from CGL.models import Round, Season
+from django.core.management.base import BaseCommand
+from django.template import Context
+from django import template
+
+from CGL.models import Round, Season, School, SchoolAuth
 from CGL.settings import current_seasons as current_season_names
 from CGL.season_management import get_actively_participating_schools
+
+template_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "email_templates")
 
 class Command(BaseCommand):
     args = ''
     help = '''Renders an email to rendered_email.txt containing a formatted email to be sent out.'''
 
     def handle(self, *args, **options):
+        choice = raw_input("Select a template:\n1. intro_email\n2. weekly_email")
+        if choice == "1":
+            school_name = raw_input("Which school?")
+            self.render_introductory_email(school_name)
+        elif choice == "2":
+            self.render_weekly_email()
+
+
+    def render_introductory_email(self, school_name):
+        with open(os.path.join(template_dir, "intro_email.txt")) as f:
+            t = template.Template(f.read())
+
+        school = School.objects.get(name__contains=school_name)
+        school_auth = SchoolAuth.objects.get(school=school)
+
+        from CGL.captain_auth import AUTH_KEY_COOKIE_NAME
+        context = Context(locals())
+        self.stdout.write(t.render(context))
+
+
+    def render_weekly_email(self):
+        with open(os.path.join(template_dir, "weekly_email.txt")) as f:
+            t = template.Template(f.read())
+
         try:
             previous_round_date = Round.objects.get_previous_round().date
             next_round_date = Round.objects.get_next_round().date
         except Exception as e:
-            self.stdout.write('Warning: Couldn\'t find previous or next round date.')
+            self.stderr.write('Warning: Couldn\'t find previous or next round date.')
 
         current_seasons = [Season.objects.get(name=s) for s in current_season_names]
         participating_schools = get_actively_participating_schools(current_season_names)
@@ -29,15 +58,4 @@ class Command(BaseCommand):
 
 
         c = Context(locals())
-        
-        try:
-            email_template = 'weekly_email'
-            t = loader.get_template(email_template)
-        except Exception as e:
-            self.stdout.write(str(e))
-            raise CommandError('Email template not found')
-
-        with open('templates/rendered-email', 'w') as f:
-            f.write(t.render(c))
-
-        self.stdout.write('Email rendered to templates/rendered-email.\n')
+        self.stdout.write(t.render(c))
