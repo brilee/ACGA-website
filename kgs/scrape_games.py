3464683fb82c01c2b03bb8cace4bc82fb3cdcbf0
Brian Lee <brian.kihoon.lee@gmail.com>
2015-10-18 01:04:08 -0400

    Handle when KGS returns zero games

--- kgs/scrape_games.py
+++ kgs/scrape_games.py
@@ -63,8 +63,11 @@ def process_KGS_game_row(row):
 
 def process_KGS_page(rawtext):
     soup = BeautifulSoup(rawtext)
-    rows = soup.find_all("table")[0].find_all("tr")[1:]
-    return map(process_KGS_game_row, rows)
+    try:
+        rows = soup.find_all("table")[0].find_all("tr")[1:]
+        return map(process_KGS_game_row, rows)
+    except IndexError:
+        return []
 
 def get_KGS_games(username, year, month):
     return process_KGS_page(get_raw_KGS_data(username, year, month))
