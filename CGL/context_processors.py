2551417b8a290ce1bd28cd77b8d332b1f144a3fe
Brian Lee <brian.kihoon.lee@gmail.com>
2015-11-29 15:21:07 -0500

    add ogs caching

--- CGL/context_processors.py
+++ CGL/context_processors.py
@@ -1,11 +1,25 @@
 # You must include a reference to these context processors in settings.py,
 # under "TEMPLATE_CONTEXT_PROCESSORS".
 
+from django.core.cache import cache
 
 from models import Season
 from settings import current_seasons
 
 from ogs.client import get_ladder_top_players
+
+def django_caching(f):
+    def wrapped(*args):
+        cached = cache.get(str(args))
+        if cached:
+            return cached
+        else:
+            result = f(*args)
+            cache.set(str(args), result, 60*10)
+            return result
+    return wrapped
+
+get_ladder_top_players = django_caching(get_ladder_top_players)
 
 
 def sidebar_CGL(request):
