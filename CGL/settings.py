ef6b7ef986a1f52154787203761ab3c1ac3abfe4
Brian Lee <brian.kihoon.lee@gmail.com>
2016-03-26 15:32:26 -0400

    Update season to S5 Championship

--- CGL/settings.py
+++ CGL/settings.py
@@ -1,3 +1,3 @@
 # this one controls all the seasons that display on the Results tab
-current_seasons = ['Season Five']
+current_seasons = ['Season Five Championship']
 assert type(current_seasons) == list
