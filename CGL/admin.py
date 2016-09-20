7ab64a25f718dd7c9456e0e120a1be7de5477e48
Brian Lee <brian.kihoon.lee@gmail.com>
2016-09-20 12:24:37 -0400

    Add admin filtering for Team.players

--- CGL/admin.py
+++ CGL/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from singleton_models.admin import SingletonModelAdmin
 from CGL.models import * 
@@ -10,11 +11,21 @@ class PlayerProfileInline(admin.StackedInline):
 class GameAdmin(admin.ModelAdmin):
     fields = ('match', 'gamefile', 'white_school', 'white_player', 'black_player', 'board')
 
+class TeamAdmin(admin.ModelAdmin):
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        # hack to get the actual instance being referred to by the admin view.
+        team_id = filter(bool, request.path.split('/'))[-1]
+        team = Team.objects.get(id=team_id)
+        if db_field.name == "players":
+             kwargs["queryset"] = Player.objects.filter(school=team.school)
+        return super(TeamAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+
 admin.site.register(School)
 admin.site.register(Player)
 admin.site.register(Season)
 admin.site.register(CurrentSeasons, SingletonModelAdmin)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Round)
 admin.site.register(Match)
 admin.site.register(Game, GameAdmin)
